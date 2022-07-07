49baecb408c3a89a5babbade3bab20ef44845f95
Chrispolanco <chrispolanco@sbcglobal.net>
2022-07-06 22:41:02 -0700

    card list created

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { Component } from 'react';
+import CardList from './components/card-list/card-list.component';
+
 import './App.css';
 
 class App extends Component {
@@ -47,15 +49,16 @@ class App extends Component {
           placeholder='Search Monsters' 
           onChange={ onSearchChange }
         />
-        {
+        {/*
           filteredMonsters.map((monster) => {
             return (
              <div key={monster.id}> 
               <h1> {monster.name} </h1>
             </div>
-            )
+            ); 
           })
-        }
+        */}
+        <CardList/> 
       </div>
     );
   }
